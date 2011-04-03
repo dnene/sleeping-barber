ab7ab25e8edf633ea3670671a557dc5b506eb940
Dhananjay Nene <dhananjay.nene@gmail.com>
2011-04-03 06:38:43 +0530

    initial import

---
+++ src/room.erl
@@ -0,0 +1,53 @@
+-module(room).
+-author('Dhananjay Nene').
+-behaviour(gen_server).
+
+-export([start/0, enter/1, get_next/0]).
+-export([init/1, handle_call/3, handle_cast/2, 
+	 handle_info/2, terminate/2, code_change/3]).
+
+-define(ROOM_SIZE, 3).
+
+start() ->
+    gen_server:start_link({local,?MODULE}, ?MODULE, [], []).
+enter(CustomerId) ->
+    gen_server:cast(?MODULE, {customer,CustomerId}).
+get_next() ->
+    gen_server:call(?MODULE, next).
+
+init([]) ->
+    {ok, queue:new()}.
+
+handle_cast({customer,CustomerId}, Queue) ->    
+    L = queue:len(Queue),
+    if
+	L >= ?ROOM_SIZE ->
+	    io:format("turning away customer                ~p~n",
+		     [CustomerId]),
+	    {noreply, Queue};
+	true ->
+	    io:format("seating customer in the waiting room ~p~n",
+		     [CustomerId]),
+	    barber:notify(),
+	    {noreply, queue:in(CustomerId,Queue)}
+    end.
+	    
+handle_call(next,_From, Queue) ->
+    % Barber is now free. Send him the next customer 
+    case queue:out(Queue) of 
+	{empty, _} -> {reply, empty, Queue};
+	{{value,Id},Remaining} -> {reply,{customer,Id},Remaining}
+    end.			    
+
+handle_info(_Message, State) ->
+    % No info messages implemented. Ignore all
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVersion, State, _Extra) ->
+    {ok, State}.
+
+
+
